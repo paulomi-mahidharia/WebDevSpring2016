743882b7bd1e1298b40dd3ab6d0d066a1db409a8
paulomimahidharia <mahidharia.p@husky.neu.edu>
2016-03-18 00:30:33 -0400

    Modified forms.view.html and changed the config for the same.

--- public/assignment/client/js/config.js
+++ public/assignment/client/js/config.js
@@ -19,7 +19,8 @@
             })
             .when("/forms", {
                 templateUrl: "views/forms/forms.view.html",
-                controller: "FormController"
+                controller: "FormController",
+                controllerAs: "model"
             })
             .when("/fields", {
                 templateUrl: "views/forms/form-fields.view.html",
