7abc3a56cc919e77b84ee5ae96780cfce3f004c2
JoseAnnunziatoNeu <jga@ccs.neu.edu>
2016-03-23 15:39:04 -0400

    file upload experiment

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.bodyParser({ uploadDir: './public/uploads' }));
+
 app.use(multer());
 
 app.use(session({
@@ -28,6 +30,30 @@ require("./public/assignment/server/app.js")(app, uuid);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+
+
+function saveFile(req, callback)
+{
+    var path = req.files.someFile.path;
+    var imagePage = path;
+
+    if (path.indexOf("\\") > -1)
+        path = path.split("\\");
+    else
+        path = path.split("/");
+    fileName = path[path.length - 1];
+    var thm = 'thm_' + fileName;
+}
+
+
+
+app.post('/upload/file', function (req, res) {
+    saveFile(req, function () {
+        res.send(200);
+        //res.redirect("/#/" + username + "/profile");
+    });
+});
 
 app.listen(port, ipaddress, function () {
     console.log("Server is listening on: " + ipaddress + ":" + port);
