c80e372a160e6028c056dbcc0c5af1291c0dfcae
paulomimahidharia <mahidharia.p@husky.neu.edu>
2016-03-23 20:49:50 -0400

    Restored latest server.js

--- server.js
+++ server.js
@@ -1,37 +1,60 @@
 var express       = require('express');
 var app           = express();
+
 var bodyParser    = require('body-parser');
 var multer        = require('multer');
+var uuid          = require('node-uuid');
 var cookieParser  = require('cookie-parser');
 var session       = require('express-session');
-var uuid          = require('node-uuid');
-
-console.log("secret");
-console.log(process.env.PASSPORT_SECRET);
-
-app.use(bodyParser.json()); // for parsing application/json
-
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-
-
-app.use(multer());
-
-app.use(session({ secret: process.env.PASSPORT_SECRET , saveUninitialized: true, resave: true}));
-
-app.use(cookieParser());
 
 app.use(express.static(__dirname + '/public'));
 
-require("./public/experiments/omdb/server/app.js")(app);
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.bodyParser({ uploadDir: './public/uploads' }));
+
+app.use(multer());
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.PASSPORT_SECRET
+}));
+
+app.use(cookieParser());
+
+
 require("./public/assignment/server/app.js")(app, uuid);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get('/sayHello', rootRequest);
-function rootRequest(req, res)
+
+
+function saveFile(req, callback)
 {
-    res.send('hello world');
+    var path = req.files.someFile.path;
+    var imagePage = path;
+
+    if (path.indexOf("\\") > -1)
+        path = path.split("\\");
+    else
+        path = path.split("/");
+    fileName = path[path.length - 1];
+    var thm = 'thm_' + fileName;
 }
 
-app.listen(port, ipaddress);
 
+
+app.post('/upload/file', function (req, res) {
+    saveFile(req, function () {
+        res.send(200);
+        //res.redirect("/#/" + username + "/profile");
+    });
+});
+
+app.listen(port, ipaddress, function () {
+    console.log("Server is listening on: " + ipaddress + ":" + port);
+});
