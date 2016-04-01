57dcd8de1c0d7a947fac01a741f6fb35ac587cd1
paulomimahidharia <mahidharia.p@husky.neu.edu>
2016-04-01 18:09:30 -0400

    Added connection string for openshift in server.js

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ var mongoose      = require("mongoose");
 // setup a local connection string
 var connectionString = 'mongodb://127.0.0.1:27017/assignment';
 
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+
 //connect to database
 var db = mongoose.connect(connectionString);
 
