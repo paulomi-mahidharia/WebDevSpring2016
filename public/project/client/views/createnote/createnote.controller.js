9bf4befad68af37567094616b40aa3f665e8c166
paulomimahidharia <mahidharia.p@husky.neu.edu>
2016-04-16 21:44:52 -0400

    Now pushing creation date when a note is created!

--- public/project/client/views/createnote/createnote.controller.js
+++ public/project/client/views/createnote/createnote.controller.js
@@ -22,12 +22,18 @@
 
         function addNote(note){
 
-            NoteService.createNoteForUser($rootScope.currentUser._id, note)
+            var currentUser = $rootScope.currentUser;
+            var currentUserId = currentUser._id;
+
+            note.createdBy = currentUser;
+            note.createdDate = new Date();
+
+            NoteService.createNoteForUser(currentUserId, note)
                 .then(
                     function(response) {
 
                         $location.url("/editnote/"+response.data._id);
-                        //return FormService.findAllFormsForUser($rootScope.currentUser._id)
+
                     });
 
 
