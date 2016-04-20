0ab6fab539c994f4112fbaf545e4a4cb35b27cd8
paulomimahidharia <mahidharia.p@husky.neu.edu>
2016-04-20 01:44:12 -0400

    Fixed share notes page

--- public/project/client/assets/js/config.js
+++ public/project/client/assets/js/config.js
@@ -265,27 +265,7 @@
             });
     }
 
-    /*function checkLoggedIn(UserService, $q, $location) {
 
-        var deferred = $q.defer();
-
-        UserService.getCurrentUser().then(function (response) {
-
-            var currentUser = response.data;
-
-            if (currentUser) {
-                UserService.setCurrentUser(currentUser);
-                deferred.resolve();
-
-            } else {
-
-                deferred.reject();
-                $location.url("/home");
-            }
-        });
-
-        return deferred.promise;
-    }*/
     function checkLoggedIn($q, $location, $http, $rootScope, $timeout) {
 
         var deferred = $q.defer();
